551852a985b43417e582e42fc75835e16805b975
aloha <aloha@aloha.(none)>
2012-04-27 16:46:36 +0800

    update header

--- Mergesort.h
+++ Mergesort.h
@@ -13,33 +13,24 @@ void copy_array(int source[],int dest[],int floor,int ceiling){
 }
 
 void merge(int a[],int floor,int middle,int ceiling){
-	int b[20] = {0};
-	int c[20] = {0};
-	copy_array(a,b,floor,middle);
-	copy_array(a,c,middle+1,ceiling);
+	int temp_1[20] = {0};
+	int temp_2[20] = {0};
+	copy_array(a,temp_1,floor,middle);
+	copy_array(a,temp_2,middle+1,ceiling);
 
-	int i=0,j=0,k=0;
-	while(b[j] < INT_MAX && c[k] < INT_MAX){
-		result[i++] = b[j] < c[k]? b[j++]:c[k++];
+	int j=0,k=0;
+	while(temp_1[j] < INT_MAX && temp_2[k] < INT_MAX){
+		a[floor++] = temp_1[j] < temp_2[k]? temp_1[j++]:temp_2[k++];
 	}
-	if(b[j] == INT_MAX){
-		while(c[k] != INT_MAX){
-			result[i++] = c[k++];
+	if(temp_1[j] == INT_MAX){
+		while(temp_2[k] != INT_MAX){
+			a[floor++] = temp_2[k++];
 		}
 	}
-	else if(c[k] == INT_MAX){
-		while(b[j] != INT_MAX){
-			result[i++] = b[j++];
+	else if(temp_2[k] == INT_MAX){
+		while(temp_1[j] != INT_MAX){
+			a[floor++] = temp_1[j++];
 		}
-	}
-	/*write the sorted result into source array to ensure
-	  each part of the source is a sorted part before we 
-	  merge the part
-	*/
-	i = 0;
-	while(i < (j+k)){
-		a[floor+i] = result[i];
-		i++;
 	}
 }
 
@@ -47,8 +38,13 @@ void mergesort(int a[],int floor,int ceiling){
 	if(floor < ceiling){
 		int q;
 		q = (floor+ceiling)/2;
+		/*merge the first half of the array,once there is only
+		  one element in the array,we can see it as a sorted 
+		  array
+		*/
 		mergesort(a,floor,q);
 		mergesort(a,q+1,ceiling);
+		/*combine two sorted array*/
 		merge(a,floor,q,ceiling);
 	}
 }
